48f8f801adf3b195badc67ec1a1952dbe8805e63
Veli-Pekka Luoto <jullesammakka@hotmail.fi>
2026-06-27 21:16:24 +0300

    t3

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TextColor } from './text-color';
 import { FontSize } from './font-size';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TextColor, FontSize],
+  imports: [RouterOutlet, TextColor, FontSize, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
@@ -13,4 +14,8 @@ export class App {
   protected readonly title = signal('angular_directives');
   color = 'yellow'
   fontSize = '20px';
+  show = false;
+  toggle() {
+    this.show = !this.show;
+  }
 }
